0f2d91a7f4b743fd90f9789ae55716145e73f406
Adityaa888 <145027209+Adityaa888@users.noreply.github.com>
2024-06-01 17:44:08 +0530

    Update Controllers.js

--- Controllers.js
+++ Controllers.js
@@ -3,8 +3,13 @@
 
 async function getTopProducts(company, category, minPrice, maxPrice) {
     const url = `${process.env.BASE_URL}/companies/${company}/categories/${category}/products/top-n?minPrice=${minPrice}&maxPrice=${maxPrice}`;
+    const token = process.env.BEARER_TOKEN;
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': Bearer ${token}
+            }
+        });
         return response.data;
     } catch (error) {
         console.error(`Error fetching top products: ${error}`);
@@ -15,8 +20,13 @@ async function getTopProducts(company, category, minPrice, maxPrice) {
 
 async function getProductDetails(company, category, productId) {
     const url = `${process.env.BASE_URL}/companies/${company}/categories/${category}/products/${productId}`;
+    const token = process.env.BEARER_TOKEN;
     try {
-        const response = await axios.get(url);
+       const response = await axios.get(url, {
+            headers: {
+                'Authorization': Bearer ${token}
+            }
+        });
         return response.data;
     } catch (error) {
         console.error(`Error fetching product details: ${error}`);
